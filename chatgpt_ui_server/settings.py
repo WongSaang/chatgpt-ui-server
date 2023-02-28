83bb28e47ad19dd97ca68ca31724ec77e8da2b1d
Rafi <rafiwx@gmail.com>
2023-02-28 15:13:55 +0800

    Add APP_URL environment variable

--- chatgpt_ui_server/settings.py
+++ chatgpt_ui_server/settings.py
@@ -30,9 +30,12 @@ SECRET_KEY = 'django-insecure-__9p!i2^udts*l==hl)+6=!fi872f3ec(n%(^f-!6i$o5+7#ar
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False) == 'True'
 
-ALLOWED_HOSTS = [os.getenv('APP_URL', '*')]
+ALLOWED_HOSTS = [os.getenv('APP_DOMAIN', '*')]
 
-CSRF_TRUSTED_ORIGINS=[os.getenv('APP_URL', 'http://localhost:9000')]
+CSRF_TRUSTED_ORIGINS=[
+    'http://' + os.getenv('APP_DOMAIN', 'localhost:9000'),
+    'https://' + os.getenv('APP_DOMAIN', 'localhost:9000')
+]
 
 
 # Application definition
