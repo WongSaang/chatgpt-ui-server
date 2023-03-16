d366a9a455bbca50642dd3cf9b6ff98043b06e85
Rafi <rafiwx@gmail.com>
2023-03-16 14:35:28 +0800

    Admin: Displaying Model Fields

--- chat/admin.py
+++ chat/admin.py
@@ -2,10 +2,22 @@ from django.contrib import admin
 
 from .models import Conversation, Message, Setting
 
-admin.site.register(Conversation)
-admin.site.register(Message)
 
+@admin.register(Conversation)
+class ConversationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'topic', 'created_at')
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'get_conversation_topic', 'message', 'is_bot', 'created_at')
+
+    def get_conversation_topic(self, obj):
+        return obj.conversation.topic
+
+    get_conversation_topic.short_description = 'Conversation Topic'
+
+
+@admin.register(Setting)
 class SettingAdmin(admin.ModelAdmin):
     list_display = ('name', 'value')
-
-admin.site.register(Setting, SettingAdmin)
