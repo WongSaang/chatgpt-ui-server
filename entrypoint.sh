3a57e8b3ff3ed8247f586e047450779dfb8c8ace
Rafi <rafiwx@gmail.com>
2023-03-19 12:40:21 +0800

    Update the timeout of gunicorn workers to 60 seconds

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ python manage.py migrate
 
 python manage.py createsuperuser --no-input
 
-exec gunicorn chatgpt_ui_server.wsgi --bind 0.0.0.0:8000 --access-logfile -
+exec gunicorn chatgpt_ui_server.wsgi --timeout 60 --bind 0.0.0.0:8000 --access-logfile -
