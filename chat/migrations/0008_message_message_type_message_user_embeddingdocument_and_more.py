39fc67d3e31e4084cbf515ffbc67c6f7b478ba8f
Rafi <rafiwx@gmail.com>
2023-05-10 17:04:11 +0800

    fix(migration) Set the default value of the user foreign key of the message model to 1 to resolve the migration error

--- chat/migrations/0008_message_message_type_message_user_embeddingdocument_and_more.py
+++ chat/migrations/0008_message_message_type_message_user_embeddingdocument_and_more.py
@@ -34,7 +34,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='message',
             name='user',
-            field=models.ForeignKey(default=0, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
             preserve_default=False,
         ),
         migrations.RunPython(update_message_user),
